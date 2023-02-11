a2917224cb073a2b5700f1796d3695d21fe1954e
Jiyeol Lee <ka.felix496@gmail.com>
2023-02-11 02:20:40 -0500

    fetch posts using query service

--- src/CommentList.js
+++ src/CommentList.js
@@ -1,23 +1,9 @@
-import React, { useCallback, useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
 
-export const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([]);
-
+export const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => (
     <li key={comment.id}>{comment.content}</li>
   ));
-  const fetchComments = useCallback(async () => {
-    const res = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
-
-    setComments(res.data);
-  }, [postId]);
-
-  useEffect(() => {
-    fetchComments();
-  }, [fetchComments]);
 
   return <ul>{renderedComments}</ul>;
 };
